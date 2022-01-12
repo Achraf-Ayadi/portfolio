c5f15b8662c172c724733dca8373a2cbcb87e5ef
achraf-ayadi <ayadi.achraf2@gmail.com>
2022-01-12 17:24:47 +0100

    added hallo

--- src/components/AboutHome.js
+++ src/components/AboutHome.js
@@ -15,11 +15,11 @@ const AboutHome = () => {
             <div className='underline'></div>
           </div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-            atque incidunt eveniet nisi beatae aliquid illo magni ullam animi
-            vero, eius cum debitis hic doloribus assumenda ducimus? Ducimus,
-            expedita, maiores velit quo facilis qui hic voluptatum voluptas ea
-            officiis maxime?
+            hallo Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Obcaecati atque incidunt eveniet nisi beatae aliquid illo magni
+            ullam animi vero, eius cum debitis hic doloribus assumenda ducimus?
+            Ducimus, expedita, maiores velit quo facilis qui hic voluptatum
+            voluptas ea officiis maxime?
           </p>
 
           <p>
